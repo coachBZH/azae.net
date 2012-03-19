1347f00e2727fc98cee5ae46140fcc33f233d48c
Thomas Clavier <tom@tcweb.org>
2012-03-19 22:58:02 +0100

    ajout d'une page contact

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 gem "sinatra"
 gem "haml"
+gem "pony"
 
 group :test do
   gem "shotgun"
--- azae.rb
+++ azae.rb
@@ -1,10 +1,6 @@
-require 'rubygems'
-require 'haml'
-require 'bundler/setup'
-
-require 'sinatra/base'
 
 class Azae < Sinatra::Base
+
   configure do
     set :haml, :format => :html5
   end
@@ -16,4 +12,37 @@ class Azae < Sinatra::Base
   get '/devops' do
     haml :devops
   end
+
+  get '/contact' do
+    haml :contact
+  end
+
+  post '/contact' do
+    Pony.mail :to        => "contact@azae.net",
+              :from      => params[:email],
+              :subject   => "[Azaé.net] demande de contact, #{params[:nom]}",
+              :html_body => haml(:contact_email, {:layout => :layout_mail}),
+              :via       => :smtp
+    flash "Votre message à bien été envoyé"
+    redirect '/contact'
+  end
+
+  def styled_flash 
+    display_messages = Array.new
+    debug = ""
+    if @flash != nil
+      @flash.each do |type, messages| 
+        messages.each do |message|
+          display_messages << "  <div class='flash #{type}'>#{message}</div>\n"
+        end
+      end
+      "<div id='flash'>\n" + display_messages.join + "</div>"
+    end
+  end
+
+  def flash (message, type = :notice)
+    @flash = Hash.new if @flash == nil
+    @flash[type] = Array.new if @flash[type] == nil
+    @flash[type] << message
+  end 
 end
