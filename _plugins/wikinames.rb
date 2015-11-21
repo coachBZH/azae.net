34f64cbcb715e31f14414044f85fb5aafc2aab99
Olivier Albiez <olivier.albiez@poloper.org>
2015-11-21 22:13:52 +0100

    wikinames plugin commented out

--- _plugins/wikinames.rb
+++ _plugins/wikinames.rb
@@ -4,32 +4,32 @@
 
 require 'jekyll'
 
-module Jekyll
-  class CardLink < Jekyll::Generator
+# module Jekyll
+#   class CardLink < Jekyll::Generator
 
-    safe true
+#     safe true
 
-    def initialize(config)
-    end
+#     def initialize(config)
+#     end
 
-    def generate(site)
-      @site = site
-      #@cards = Hash[ site.collections['cards'].docs.map { |page| [ page.basename_without_ext, page.data['title'] ] } ]
-      site.collections['cards'].docs.each { |page| autolinkify page }
-    end
+#     def generate(site)
+#       @site = site
+#       #@cards = Hash[ site.collections['cards'].docs.map { |page| [ page.basename_without_ext, page.data['title'] ] } ]
+#       site.collections['cards'].docs.each { |page| autolinkify page }
+#     end
 
-    private
+#     private
 
-    def autolinkify(page)
-      page.content = page.content.gsub(/~(\w+)/) do |value|
-        ref = Regexp.last_match[1]
-        #name = @cards[ref]
-        if not name.nil? then
-          "<a href='/cards/#{ref}'>#{ref}</a>"
-        else
-          raise ArgumentError.new "Could not find card '#{ref}', Make sure the card exists and the name is correct."
-        end
-      end
-    end
-  end
-end
+#     def autolinkify(page)
+#       page.content = page.content.gsub(/~(\w+)/) do |value|
+#         ref = Regexp.last_match[1]
+#         #name = @cards[ref]
+#         if not name.nil? then
+#           "<a href='/cards/#{ref}'>#{ref}</a>"
+#         else
+#           raise ArgumentError.new "Could not find card '#{ref}', Make sure the card exists and the name is correct."
+#         end
+#       end
+#     end
+#   end
+# end
