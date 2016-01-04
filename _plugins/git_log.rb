013704ff93909688b3f298561a66da2dd9ece26e
Olivier Albiez <olivier.albiez@poloper.org>
2016-01-04 23:31:52 +0100

    contributors added on articles

---
+++ _plugins/git_log.rb
@@ -0,0 +1,21 @@
+require 'git'
+require 'pathname'
+
+module Jekyll
+
+  class GitLogGenerator < Jekyll::Generator
+    safe :true
+    priority :high
+
+    def generate(site)
+      git = Git.open(site.source)
+      root = Pathname.new site.source
+
+      site.posts.docs.each do |post|
+        relative = Pathname.new(post.path).relative_path_from(root).to_s
+        contributors = git.log.object(relative).map {|l| l.author.name }.uniq.sort
+        post.data['contributors'] = contributors
+      end
+    end
+  end
+end
